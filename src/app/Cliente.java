b22d8f6cf2b58bbb08c8ba5e39a6142d117e5ccf
roD1G0 <roodrigo.rodrigues374pintos@gmail.com>
2021-12-20 17:04:58 -0300

    Adição da classe Endereco

--- src/app/Cliente.java
+++ src/app/Cliente.java
@@ -3,25 +3,16 @@ package app;
 public class Cliente {
 	private String nome;
 	private long telefone;
-	private long CEP;
 	private boolean situacao;
 	private double renda;
-	private String pais;
-	private String cidade;
-	private String rua;
-	private int casa;
 	private int quantContas;
+	private Endereco endereco;
 	//construtor de cliente
 	public Cliente(String nome) {
 		this.nome = nome;
 		telefone = 0;
 		renda = 0;
-		CEP = 0;
 		situacao = false;
-		pais = " ";
-		cidade = " ";
-		rua = " ";
-		casa = 0;
 		quantContas = 0;
 	}
 	public String getNome() {
@@ -30,17 +21,11 @@ public class Cliente {
 	public long getTelefone() {
 		return telefone;
 	}
-	public long getCEP() {
-		return CEP;
-	}
 	public boolean getSituacao() {
 		return situacao;
 	}
 	public double getRenda() {
 		return renda;
-	}
-	public String getEndereco() {
-		return "País: "+ pais +" Cidade: "+ cidade +" Rua: "+ rua +" Número: "+casa;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -54,18 +39,6 @@ public class Cliente {
 	public void setRenda(double renda) {
 		this.renda = renda;
 	}
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-	public void setCasa(int casa) {
-		this.casa = casa;
-	}
 	//verifica qual a situacap do cliente
 	public String verificaSituacao() {
 		if(situacao) {
@@ -75,7 +48,7 @@ public class Cliente {
 	}
 	//retorna as informacoes do cliente
 	public String toString() {
-		return "Nome: "+ nome + " Telefone: "+ telefone + " Situação: "+ verificaSituacao()+ " Renda: "+ renda+ "\n"+ getEndereco();
+		return "Nome: "+ nome + " Telefone: "+ telefone + " Situação: "+ verificaSituacao()+ " Renda: "+ renda+ "\n"+ endereco.getEndereco();
 	}
 	//toda vez que o cliente abre uma conta incrementa em 1 a quantidade de contas
 	public void incrementaContas() {
---
+++ src/app/Endereco.java
@@ -0,0 +1,52 @@
+package app;
+
+public class Endereco {
+	private long CEP;
+	private String pais;
+	private String cidade;
+	private String rua;
+	private int numero;
+	
+	public Endereco(long CEP,String pais,String cidade,String rua,int numero) {
+		this.CEP = CEP;
+		this.pais = pais;
+		this.cidade = cidade;
+		this.rua = rua;
+		this.numero = numero;
+	}
+	public long getCEP() {
+		return CEP;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public void setCEP(long CEP) {
+		this.CEP = CEP;
+	}
+	public String getEndereco() {
+		return "CEP: "+ CEP +"País: "+ pais +" Cidade: "+ cidade +" Rua: "+ rua +" Número: "+numero;
+	}
+	
+
+}
